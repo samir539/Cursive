640a2b136afcc046f4a57ccaf2649112f74660a5
samir539 <samir.c.asghar@gmail.com>
2025-01-06 20:43:43 +0000

    writing to screen

--- cursive.c
+++ cursive.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -63,11 +64,28 @@ char editorReadKey(){
     return c;
 }
 
+int getCursorPosition(int *rows, int *cols){
+    char buf[32];
+    unsigned int i =0;
+    if (write(STDOUT_FILENO, "\x1b[6n",4) != 4) return -1;
+    while (i <sizeof(buf) -1) {
+        if (read(STDIN_FILENO, &buf[i],1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
+    }
+    buf[i] = '\0';
+    
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows,cols) !=2) return -1;
+    return 0;
+}
+
 int getWindowSize(int *rows, int *cols){
     struct winsize ws;
 
     if (ioctl(STDOUT_FILENO,TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
-        return -1;
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B",12) != 12) return -1;
+        return getCursorPosition(rows,cols);
     } else {
         *cols = ws.ws_col;
         *rows = ws.ws_row;
@@ -85,28 +103,60 @@ void editorProcessKeypress(){
     }
  }
 
-void editorDrawRows(){
+
+
+#define ABUF_INIT{NULL,0}
+/*** append buffer ***/
+struct abuf {
+    char *b;
+    int len;
+       
+};
+
+
+void abAppend(struct abuf *ab, const char *s, int len){
+    char *new = realloc(ab->b, ab->len+len);
+    
+    if (new == NULL) return;
+    memcpy(&new[ab->len],s,len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab){
+    free(ab->b);
+}
+
+
+void editorDrawRows(struct abuf *ab){
     int y;
     for (y=0; y<E.screenrows;y++){
-        write(STDOUT_FILENO, "-\r\n",3);
-    }
-    if (y < E.screenrows -1){
-        write(STDOUT_FILENO, "\r\n",2);
-    }
+        abAppend(ab,"~",1);
+        abAppend(ab, "\x1b[K",3);
+        if (y < E.screenrows -1){
+            abAppend(ab,"\r\n",2);
+        }
+
+     }
 }
 
 
 void editorRefreshScreen(){
-    write(STDOUT_FILENO, "\x1b[2J",4);
-    write(STDOUT_FILENO, "\x1b[H",3);
+    struct abuf ab = ABUF_INIT;
+    abAppend(&ab, "\x1b[?25l",6);
+    abAppend(&ab, "\x1b[H",3);
     
-    editorDrawRows();
-    write(STDOUT_FILENO, "\x1b[H",3);
+    editorDrawRows(&ab);
+    abAppend(&ab, "\x1b[H",3);
+    abAppend(&ab, "\x1b[?25h",6);
+    write(STDOUT_FILENO, ab.b,ab.len);
+    abFree(&ab);
 }
 
 void initEditor(){
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
+
 
 int main(){
     enableRawMode();
